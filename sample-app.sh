c1015550b02fadd8787415d4e1eccbe9df5aa43d
BEN MOUSSA <46443502+ensafbn@users.noreply.github.com>
2021-11-24 23:15:30 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -16,6 +16,7 @@ echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
+
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
